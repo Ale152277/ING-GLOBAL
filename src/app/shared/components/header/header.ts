0a968008f25b5ddd863fada26443e24d9273e6ab
Ale152277 <gsjoaquin22@gmail.com>
2026-01-07 01:04:01 -0500

    .

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements OnInit {
+  cantidadCarrito =0;
+  cantidadDeseos= 0;
+  usuarioAutenticado = false;
+  nombreUsuario = 'Guest';
+  menuAbierto = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.cargarDatos();
+  }
+
+  private cargarDatos(): void {
+    this.usuarioAutenticado = false;
+    this.cantidadCarrito =  0;
+    this.cantidadDeseos = 0;
+
+
+  }
+
+  logout():void{
+    this.usuarioAutenticado = false;
+    this.nombreUsuario = 'Guest';
+    this.router.navigate(['/auth/login']);
+  }
+
+  agregaraCarrito():void{
+    this.cantidadCarrito ++;
+  }
+
+  toggleMenu(): void {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu(): void {
+    this.menuAbierto = false;
+  }
+
+
 
 }
