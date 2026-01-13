b9b6f98a44049e003c5bf1323c3e5e651f9645e3
Ale152277 <gsjoaquin22@gmail.com>
2026-01-13 13:04:19 -0500

    .

---
+++ src/app/features/productos/components/filtro-productos/filtro-productos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-productos',
+  imports: [],
+  templateUrl: './filtro-productos.html',
+  styleUrl: './filtro-productos.css',
+})
+export class FiltroProductos {
+
+}
